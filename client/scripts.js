02afb08d030b3288f8c551910fdf0348003333e0
Matt <ghi8.matteo@gmail.com>
2022-03-25 16:11:03 +0100

    Improvements on the UI. Created all the pieces and initialized the board

--- client/scripts.js
+++ client/scripts.js
@@ -1,5 +1,20 @@
 //Client file connecting to the server, sending user input and displaying the opponent's output
 
+import { io } from "socket.io-client";
+
+const socket = io('http://192.168.1.94:3000', {
+    transportOptions: {
+        polling: {
+            extraHeaders: {
+            "my-custom-header": "abcd"
+            }
+        }
+    }
+});
+
+
+
+
 const root = document.getElementById('root');
 
 const size = 80;
@@ -7,9 +22,123 @@ const size = 80;
 var checkClr = 'white';
 var color1 = 'white';
 var color2 = 'black';
+var room;
+var user;
 
 
-//FUNCTIONS--------------------------------------------------------------------
+//Create Board Matrix:
+var boardMatrix = new Array(8);
+
+for (let i = 0; i < boardMatrix.length; i++) {
+    boardMatrix[i] = new Array(8);
+}
+
+console.log(boardMatrix);
+
+
+
+//FUNCTIONS-&-CLASSES------------------------------------------------------------------
+
+
+class Piece {
+    constructor(_type, _player, _y, _x) {
+        this.type = _type;
+        this.player = _player
+        this.y = _y;
+        this.x = _x;
+    }
+
+    destinations() {
+        let destinationsList = [];  //put objects containing pair of coordinates of the possible destinations
+        //then find a way to get that checker and highlight it and make it clickable, and when you click it the piece moves there.
+    }
+    //KING
+
+    //QUEEN
+
+    //BISHOP
+
+    //KNIGHT
+
+    //ROOK
+
+    //PAWN
+
+
+
+}
+
+
+function initializeBoard() {
+    
+    //Set pawns
+    for(let i = 0; i < 8; i++){
+        boardMatrix[1][i] = new Piece('pawn', 'white', 1, i);
+        boardMatrix[6][i] = new Piece('pawn', 'black', 6, i);
+    }
+
+    //Set Rooks
+    boardMatrix[0][0] = new Piece('rook', 'white', 0, 0);
+    boardMatrix[0][7] = new Piece('rook', 'white', 0, 7);
+    boardMatrix[7][0] = new Piece('rook', 'black', 7, 0);
+    boardMatrix[7][7] = new Piece('rook', 'black', 7, 7);
+
+    //Set Knights
+    boardMatrix[0][1] = new Piece('knight', 'white', 0, 1);
+    boardMatrix[0][6] = new Piece('knight', 'white', 0, 6);
+    boardMatrix[7][1] = new Piece('knight', 'black', 7, 1);
+    boardMatrix[7][6] = new Piece('knight', 'black', 7, 6);
+
+    //Set Bishops
+    boardMatrix[0][2] = new Piece('bishop', 'white', 0, 2);
+    boardMatrix[0][5] = new Piece('bishop', 'white', 0, 5);
+    boardMatrix[7][2] = new Piece('bishop', 'black', 7, 2);
+    boardMatrix[7][5] = new Piece('bishop', 'black', 7, 5);
+
+    //Set Kings
+    boardMatrix[0][3] = new Piece('king', 'white', 0, 3);
+    boardMatrix[7][3] = new Piece('king', 'black', 7, 3);
+
+    //Set Queens
+    boardMatrix[0][4] = new Piece('queen', 'white', 0, 4);
+    boardMatrix[7][4] = new Piece('queen', 'black', 7, 4);
+    
+
+    //Old, just setting names, the code above actually creates each piece object.
+    /*
+    //Set pawns
+    for(let i = 0; i < 8; i++){
+        boardMatrix[1][i] = "my-pawn";
+        boardMatrix[6][i] = "enemy-pawn";
+    }
+
+    //Set Rooks
+    boardMatrix[0][0] = "my-rook";
+    boardMatrix[0][7] = "my-rook";
+    boardMatrix[7][0] = "enemy-rook";
+    boardMatrix[7][7] = "enemy-rook";
+
+    //Set Knights
+    boardMatrix[0][1] = "my-knight";
+    boardMatrix[0][6] = "my-knight";
+    boardMatrix[7][1] = "enemy-knight";
+    boardMatrix[7][6] = "enemy-knight";
+
+    //Set Bishops
+    boardMatrix[0][2] = "my-bishop";
+    boardMatrix[0][5] = "my-bishop";
+    boardMatrix[7][2] = "enemy-bishop";
+    boardMatrix[7][5] = "enemy-bishop";
+
+    //Set Kings
+    boardMatrix[0][3] = "my-king";
+    boardMatrix[7][3] = "enemy-king";
+
+    //Set Queens
+    boardMatrix[0][4] = "my-queen";
+    boardMatrix[7][4] = "enemy-queen";
+    */
+}
 
 function drawBoard(){
     const board = document.createElement('div');
@@ -26,9 +155,15 @@ function drawBoard(){
         for(let j = 0; j < 8; j++){
             let newDiv = document.createElement('div');
             newDiv.setAttribute('class', 'checker-div');
+            newDiv.setAttribute('data-y', 8-i-1);
+            newDiv.setAttribute('data-x', j);
             newDiv.setAttribute('style', `
                 background-color: ${checkerColor()};
             `)
+            newDiv.addEventListener('click', () => {
+                //socket.emit();
+                console.log(newDiv.dataset);
+            });
             boardRow.appendChild(newDiv);
         }
         checkerColor();
@@ -45,7 +180,121 @@ function checkerColor() {
     }
 }
 
+//Connection Panel
+function createConnectionPanel(aP){
+    //Row with all the connectivity features
+    const connectionRow = document.createElement('div');
+    connectionRow.setAttribute('id', 'connection-row');
+    aP.appendChild(connectionRow);
+    
+    //'Room:' Text
+    const roomText = document.createElement('p');
+    const roomText_text = document.createTextNode('Room:');
+    roomText.append(roomText_text);
+    connectionRow.appendChild(roomText);
+    //Room input text
+    const roomInputText = document.createElement('p');
+    roomInputText.setAttribute('id', 'room-input-text');
+    roomInputText.setAttribute('class', 'hidden');
+    connectionRow.appendChild(roomInputText);
 
+    //Input Box
+    const roomInput = document.createElement('input');
+    roomInput.setAttribute('id', 'room-input');
+    connectionRow.appendChild(roomInput);
+
+    //Join/Quit Button
+    const roomInputButton = document.createElement('button');
+    const roomInputButton_text = document.createTextNode('JOIN');
+    roomInputButton.append(roomInputButton_text);
+    roomInputButton.setAttribute('class', 'button');
+    connectionRow.appendChild(roomInputButton);
+
+    //Join or Quit Room
+    roomInputButton.onclick = () => {
+
+        if(!room){
+            if(roomInput.value){
+                room = roomInput.value;
+                roomInputText.innerText += room;
+                roomInputButton.innerText = 'QUIT';
+                roomInput.setAttribute('class', 'hidden');
+                roomInputText.removeAttribute('class');
+            } else {
+                alert("Room field can't be empty");
+            }
+        } else {
+            room = "";
+            roomInputText.innerText = "";
+            roomInputButton.innerText = 'JOIN';
+            roomInput.removeAttribute('class');
+            roomInputText.setAttribute('class', 'hidden');
+            roomInput.value = "";
+        }
+        
+    };
+}
+
+//Username Panel
+function createUsernamePanel(aP){
+    //Row with all the connectivity features
+    const usernameRow = document.createElement('div');
+    usernameRow.setAttribute('id', 'username-row');
+    aP.appendChild(usernameRow);
+    
+    //'Username:' Text
+    const usernameText = document.createElement('p');
+    const usernameText_text = document.createTextNode('User:');
+    usernameText.append(usernameText_text);
+    usernameRow.appendChild(usernameText);
+    //Username input text
+    const usernameInputText = document.createElement('p');
+    usernameInputText.setAttribute('id', 'username-input-text');
+    usernameInputText.setAttribute('class', 'hidden');
+    usernameRow.appendChild(usernameInputText);
+
+    //Input Box
+    const usernameInput = document.createElement('input');
+    usernameInput.setAttribute('id', 'username-input');
+    usernameRow.appendChild(usernameInput);
+
+    //Join/Quit Button
+    const usernameInputButton = document.createElement('button');
+    const usernameInputButton_text = document.createTextNode('SET');
+    usernameInputButton.append(usernameInputButton_text);
+    usernameInputButton.setAttribute('class', 'button');
+    usernameRow.appendChild(usernameInputButton);
+
+    //Set Username
+    usernameInputButton.onclick = () => {
+
+        if(!user){
+            if(usernameInput.value){
+                user = usernameInput.value;
+                usernameInputText.innerText = user;
+                usernameInputButton.innerText = "UPDATE";
+                usernameInput.setAttribute('class', 'hidden');
+                usernameInputText.removeAttribute('class');
+            } else {
+                alert("User field can't be empty");
+            }
+        } else {
+            user = "";
+            usernameInputText.innerText = "";
+            usernameInputButton.innerText = 'SET';
+            usernameInput.removeAttribute('class');
+            usernameInputText.setAttribute('class', 'hidden');
+            usernameInput.value = "";
+        }
+
+
+    };
+}
+
+//Game Panel (Game status, whose turn is it, timer)
+function createGamePanel() {
+
+}
 
 //Actions Panel
 function createActionsPanel(){
@@ -53,33 +302,105 @@ function createActionsPanel(){
     actionsPanel.setAttribute('id','actions-panel')
     root.appendChild(actionsPanel);
 
-    const connectionRow = document.createElement('div');
-    connectionRow.setAttribute('id', 'connection-row');
-    actionsPanel.appendChild(connectionRow);
-
-    const roomInputText = document.createElement('p');
-    const roomInputText_text = document.createTextNode('Room:');
-    roomInputText.append(roomInputText_text);
-    roomInputText.setAttribute('class', 'text');
-    connectionRow.appendChild(roomInputText);
-
-    const roomInput = document.createElement('input');
-    roomInput.setAttribute('id', 'room-input');
-    connectionRow.appendChild(roomInput);
-
-    const roomInputJoinButton = document.createElement('button');
-    const roomInputJoinButton_text = document.createTextNode('JOIN');
-    roomInputJoinButton.append(roomInputJoinButton_text);
-    roomInputJoinButton.setAttribute('class', 'button');
-    connectionRow.appendChild(roomInputJoinButton);
+    createUsernamePanel(actionsPanel);
+    createConnectionPanel(actionsPanel);
+    //createGamePanel(actionsPanel);
+    //createGraveyard(actionsPanel);
 
 }
 
+//Activity Log
 function createActivityLog(){
     const activityLog = document.createElement('div');
     activityLog.setAttribute('id','activity-log')
     root.appendChild(activityLog);
 }
+
+//Draw Pieces' names intead of using images for now
+function drawAllPieces() {
+    
+    for(let j = 0; j < 8; j++) {       //j=y, 
+        for(let i = 0; i < 8; i++) {   //i=x.
+            if(boardMatrix[j][i]) {
+                drawPiece(boardMatrix[j][i].type, boardMatrix[j][i].player, boardMatrix[j][i].y, boardMatrix[j][i].x);
+            }
+        }
+    }
+}
+
+//Get a specific Checker based on the coordinates
+function getChecker(_y, _x) {
+    for(let i = 0; i < allCheckers.length; i++) {
+        if(allCheckers[i].dataset.y == _y && allCheckers[i].dataset.x == _x) {
+            return allCheckers[i];
+        }
+    }
+}
+
+function drawPiece(_type, _player, _y, _x){
+    //Get the target checker object
+    let targetChecker = getChecker(_y, _x);
+    //if(!targetChecker){return;}   //Skip if it's undefined.
+    let pieceLetter;
+    let piece = document.createElement('div');
+    //Set Color
+    if(_player == "white"){
+        var bgColor = 'white';
+        var textColor = 'black';
+    } else {
+        var bgColor = 'black';
+        var textColor = 'white';
+    }
+    piece.setAttribute('style', `
+        width: 29px;
+        height: 29px;
+        margin-left: 24px;
+        margin-top: 24px;
+        background-color: ${bgColor};
+        border: solid ${textColor} 2px;
+        text-align: center;
+        border-radius: 50%;
+        color: ${textColor};
+    `)
+
+    //Set Letter to identify the type of piece
+    switch (_type) {
+        case 'pawn':
+            pieceLetter = 'P';
+            break;
+        case 'king':
+            pieceLetter = 'KK';
+            break;
+        case 'queen':
+            pieceLetter = 'Q';
+            break;
+        case 'bishop':
+            pieceLetter = 'B';
+            break;
+        case 'knight':
+            pieceLetter = 'K';
+            break;
+        case 'rook':
+            pieceLetter = 'R';
+            break;
+        default:
+            pieceLetter = 'X';
+            console.log('Error in type of piece. Assigned not identified value "X".');
+    }
+    let textNode = document.createTextNode(pieceLetter);
+    let text = document.createElement('p');
+    text.setAttribute('style', 'margin-top: 5px;')
+
+    //Append textNote to text, text to piece and piece to checker
+    text.append(textNode);
+    piece.append(text);
+    targetChecker.append(piece);
+}
+
+//EVENTS--------------------------------------------------------
+
+
+
 
 
 //CODE----------------------------------------------------------
@@ -88,7 +409,24 @@ drawBoard();
 createActionsPanel();
 createActivityLog();
 
+initializeBoard();
+
+const allCheckers = document.getElementsByClassName('checker-div');
+//var a = getChecker(7, 7);
+//a.setAttribute('style', 'background-color: red;');
+
+
+//drawPiece('rook', 'white', 3, 3);
+drawAllPieces();
 
 
 
 
+
+//TO-DO-LIST------------------------------
+/*
+
+Work on the createGamePanel() function.
+
+
+*/
--- server/server.js
+++ server/server.js
@@ -1,5 +1,106 @@
 //Server file containing all the logic, handling the connection and writing data to the database
+//SETUP
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
+
+app.use(cors());
+
+const io = require('socket.io')(3000, {
+    cors: {
+        origins: ["*"],
+
+        handlePreflightRequest: (req, res) => {
+            res.writeHead(200, {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Methods": "GET,POST",
+              "Access-Control-Allow-Headers": "my-custom-header",
+            });
+            res.end();
+        }
+    }
+});
+
+
+var game = {
+    room: "",
+    playerWhite: "",
+    playerBlack: "",
+    start: false,
+    turn: "white"
+}
+
+
+//FUNCTIONS-&-CLASSES-----------------------------------------------------------------------------
+
+class Piece {
+    constructor(_type, _x, _y) {
+        this.type = _type;
+        this.x = _x;
+        this.y = _y;
+    }
+
+    //Define the moves for all types of pieces with a SWITCH statement to allow only the right ones
+    
+    //EXAMPLE:
+    //Move knight
+    /*
+    if(type === 'knight'){
+        //move like a knight;
+    }
+    */
+
+    //KING
+
+    //QUEEN
+
+    //BISHOP
+
+    //KNIGHT
+
+    //ROOK
+
+    //PAWN
+
+
+
+}
+
+
+//EVENTS------------------------------------------------------------------------------------------
+
+io.on('connection', socket => {
+    console.log(socket.id);
+    socket.on('custom-test', () => {
+        console.log('Custom event received correctly');
+    })
+});
+
+
+//CODE--------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TO-DO-LIST------------------------------
+/*
+
+
+
+
+*/
 
 
 
